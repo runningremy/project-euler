22adc37444b3305602d6c3427e56c9f4c0132307
theo <theodore.pynos@gmail.com>
2026-08-02 22:12:54 -0400

    Solved PE1 with Rust

--- rust/pe1.rs
+++ rust/pe1.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, World!");
+    let mut i = 1;
+    let mut j = 0;
+    while i < 1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            j = j + i;
+            i += 1;
+        } else {
+            i += 1;
+        }
+    }
+    println!("{}", j);
 }
