d2a9913c7ef93463f6601801e7d76aba2fbf971a
theo <theodore.pynos@gmail.com>
2026-08-02 22:29:12 -0400

    Solved PE2 with Python

---
+++ python/pe2.py
@@ -0,0 +1,14 @@
+def main():
+    even_fibs = [1,2];
+    while even_fibs[-1] < 4000000:
+        x = even_fibs[-1] + even_fibs[-2];
+        even_fibs.append(x);
+    even_fibs.pop();
+    j = 0;
+    for i in even_fibs:
+        if i % 2 == 0:
+            j = j + i;
+        else:
+            pass;
+    print(j);
+main()
