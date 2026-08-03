a9afe56ac100eb446340eb5ba083edeee61b8192
theo <theodore.pynos@gmail.com>
2026-08-02 22:05:04 -0400

    Solved PE1 in C++

--- cpp/pe1.cpp
+++ cpp/pe1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	int i = 1;
+	int j = 0;
+	while (i < 1000) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			j = j + i;
+			i++;
+		} else {
+			i++;
+		}
+	}
+	std::cout << j << std::endl;
+	//std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
