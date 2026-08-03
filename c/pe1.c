2034a5ba7f78f7a58c2b45080a947ec4ef928cfa
theo <theodore.pynos@gmail.com>
2026-08-02 21:45:07 -0400

    Solved PE1 in C

--- c/pe1.c
+++ c/pe1.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-main()
+void main(void)
 {
-
+	int i, j;
+	i = 1;
+	j = 0;
+	while (i < 1000) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			j = j + i;
+			i++;
+		} else {
+			i++;
+		}
+	}
+	printf("%d\n", j);
 }
- 
